61d181ce40ac8355828db88553329c3147aa6ad0
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2021-08-13 10:34:25 +0530

    Create Ex 3.1.java

---
+++ Programming-in-Java/Week 3/Ex 3.1.java
@@ -0,0 +1,23 @@
+//To the generation of Fibonacci numbers.
+import java.util.Scanner; //This package for reading input
+public class Fibonacci { 
+    
+    public static void main(String args[]) { 
+	 Scanner sc = new Scanner(System.in);
+	 int n=sc.nextInt(); //Read an integer
+        System.out.println(fib(n)); //Generate and print the n-th Fibonacci                
+                                     //number
+    } 
+ static int fib(int n) {
+   
+//complete the code segment to find the nth Fibonacci number in the Fibonacci sequence and return the value. Write the function recursively.
+    int fib=0,f1=-1,f2=1;
+     for(int i=0;i<n;i++)
+       {
+         fib=f1+f2;
+         f1=f2;
+         f2=fib;
+       }
+    return fib;
+}
+}
